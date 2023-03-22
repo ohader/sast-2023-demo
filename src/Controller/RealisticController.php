25ce0283d00e7ff451805e243b722a4c2fc4a306
Oliver Hader <oliver@typo3.org>
2023-03-22 08:38:49 +0100

    Refine RealisticController

--- src/Controller/RealisticController.php
+++ src/Controller/RealisticController.php
@@ -16,7 +16,7 @@ final class RealisticController
 
     public function showAction(Request $request): Response
     {
-        $id = $request->request->get('id') ?? $request->query->get('id') ?? 0;
+        $id = $request->get('id') ?? 0;
         $queryBuilder = $this->connection->createQueryBuilder();
         $result = $queryBuilder
             ->select('*')
@@ -24,7 +24,8 @@ final class RealisticController
             ->where($queryBuilder->expr()->eq('id', $id))
             ->executeQuery();
         $item = $result->fetchAssociative();
-        $content = $this->twig->render('show.twig', ['item' => $item, 'id' => $id]);
+        $variables = ['item' => $item, 'id' => $id];
+        $content = $this->twig->render('show.twig', $variables);
         $headers = ['Content-Type' => 'text/html', 'X-App-Id' => $id];
         return new Response($content, Response::HTTP_OK, $headers);
     }
